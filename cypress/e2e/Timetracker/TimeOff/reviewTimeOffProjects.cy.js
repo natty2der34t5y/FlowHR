26441921eb4409fe02bff8cea0753a99d0b7458e
jpkwebmex <jorpalkoh@hotmail.com>
2023-09-19 15:23:24 -0600

    jp-1909215233

---
+++ cypress/e2e/Timetracker/TimeOff/reviewTimeOffProjects.cy.js
@@ -0,0 +1,32 @@
+describe('viewTimeOff', () => {
+
+    it('passes', () => {
+
+      cy.login('leaderautomation_jp@yopmail.com','Auto1989$')
+  
+      cy.wait(1000)
+
+      cy.get('[class="ant-select sc-eirqVv lhJykP ant-select-single ant-select-show-arrow"]')
+      .click()
+
+      cy.contains('Arroyo Consulting (AC)').click()
+
+      cy.get('a[href="/timetracker"]').click()
+
+      cy.get('a[href="/timetracker/reviewTimeOff"]').click()
+
+      cy.get('[class="ant-select sc-eirqVv ozpNv ant-select-single ant-select-show-arrow"]').eq(0).click()
+      
+      cy.wait(2000)
+
+      cy.get('.ant-select-selection-item').eq(2).click()
+
+      cy.get('.ant-select-item-option')
+      .contains('All projects').should('exist')
+      
+      cy.get('.ant-select-item-option')
+      .contains('Leader Automation Project JP').should('exist')
+
+    })
+  
+  })
