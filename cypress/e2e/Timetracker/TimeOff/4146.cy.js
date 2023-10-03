b41a24970a49aff6101ac7e95d70c771b56d31a9
jpkwebmex <jorpalkoh@hotmail.com>
2023-10-03 10:51:22 -0600

    jp-031023-1051

---
+++ cypress/e2e/Timetracker/TimeOff/4146.cy.js
@@ -0,0 +1,47 @@
+describe('addComment', () => {
+
+    it('passes', () => {
+
+      cy.login('sofis@gmail.com','SofiA1234!')
+  
+      cy.wait(1000)
+
+      cy.get('[class="ant-select sc-eirqVv lhJykP ant-select-single ant-select-show-arrow"]')
+      .click()
+
+      cy.contains('Arroyo Consulting (AC)').click()
+
+      cy.get('a[href="/timetracker"]').click()
+
+      cy.get('a[href="/timetracker/timeOff"]').click()
+
+      cy.get('h2').
+      contains('List of time off').
+      should('exist')
+
+      cy.get('div').
+      contains('Accepted').
+      should('exist')
+
+      cy.get('.anticon-search').
+      should("have.attr","aria-label","search"). //search click
+      click()
+
+      cy.get('input').
+      eq(3).
+      should("have.attr","placeholder","Search Reason").
+      type('License')
+
+      cy.get('span').
+      contains('Search'). //click search button
+      click()
+
+      cy.get('td').
+      contains('License').
+      should('exist')
+      
+      //
+
+    })
+  
+  })
--- cypress/e2e/Timetracker/TimeOff/selectReason.cy.js
+++ cypress/e2e/Timetracker/TimeOff/selectReason.cy.js
@@ -4,25 +4,25 @@ describe('selectReason', () => {
   
       cy.login('sofis@gmail.com','SofiA1234!') //Cypress Command for Login
   
-      cy.wait(1000) //Cypress wait of 1 second
+      cy.wait(1000) //Cypress wait of 1 second after Login
 
       cy.get('[class="ant-select sc-eirqVv lhJykP ant-select-single ant-select-show-arrow"]')
-      .click()
+      .click() //Click on Select Company Dropdown
 
-      cy.contains('Arroyo Consulting (AC)').click()
+      cy.contains('Arroyo Consulting (AC)').click() //Click on Arroyo Consulting option
 
-      cy.get('a[href="/timetracker"]').click()
+      cy.get('a[href="/timetracker"]').click() //Click on Time tracker module
 
-      cy.get('a[href="/timetracker/timeOff"]').click()
+      cy.get('a[href="/timetracker/timeOff"]').click() //Click on TIme Off section
 
-      cy.get('[class="ant-btn ant-btn-primary sc-gtsrHT cjsSOE"]').click()
+      cy.get('[class="ant-btn ant-btn-primary sc-gtsrHT cjsSOE"]').click() //Click to add time off
 
-      cy.wait(1000)
+      cy.wait(1000) //Cypress wait of 1 second after clicking to add a time off
 
-      cy.get('#addPerson_reasonId').click()
+      cy.get('#addPerson_reasonId').click() //Click on Reason Dropdown
 
       cy.get('.ant-select-dropdown')
-      .contains('License').should('exist')
+      .contains('License').should('exist')//Cypress Assertion to check License option is available
       
       cy.get('.ant-select-dropdown')
       .contains('Medical Leave').should('exist')
